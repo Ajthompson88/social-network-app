1a3ce407f3c04e8949bff781e5c19d1172a3b1dd
Ajthompson88 <Aj.thompson8888@gmail.com>
2025-03-24 18:24:32 -0400

    adding the utils for the backend and pages files for the front end

---
+++ backend/utils/dateFormat.ts
@@ -0,0 +1,13 @@
+export const customDateFormat = (timestamp: Date): any => {
+  const date = new Date(timestamp);
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true,
+  };
+  return date.toLocaleString('en-US', options);
+};
---
+++ frontend/src/pages/ThoughtDetails.tsx
@@ -0,0 +1,9 @@
+const ThoughtDetails = () => {
+  return (
+    <div>
+      <h1>Thought Details</h1>
+    </div>
+  );
+};
+
+export default ThoughtDetails;
